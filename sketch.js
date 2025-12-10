d2bd2ed376567d2ca5b4b442b2ac59fc014e4cbb
Forkentiney <jseverud@proton.me>
2025-12-10 09:08:26 -0600

    Added JS to make knobs rotate on keypress

--- sketch.js
+++ sketch.js
@@ -2,4 +2,33 @@ const screen = document.querySelector("#screen");
 const leftKnob = document.querySelector("#knob-left");
 const rightKnob = document.querySelector("#knob-right");
 
+window.addEventListener("keydown", draw);
+window.addEventListener("keyup", stopDraw);
 
+function draw(key) {
+  if (key.code == "KeyA") {
+    leftKnob.classList.add("anticlockwise");
+  } else if (key.code == "KeyS") {
+    leftKnob.classList.add("clockwise");
+  } else if (key.code == "KeyK") {
+    rightKnob.classList.add("anticlockwise");
+  } else if (key.code == "KeyL") {
+    rightKnob.classList.add("clockwise");
+  } else {
+    console.log("Not a valid keypress");
+  };
+};
+
+function stopDraw(key) {
+  if (key.code == "KeyA") {
+    leftKnob.classList.remove("anticlockwise");
+  } else if (key.code == "KeyS") {
+    leftKnob.classList.remove("clockwise");
+  } else if (key.code == "KeyK") {
+    rightKnob.classList.remove("anticlockwise");
+  } else if (key.code == "KeyL") {
+    rightKnob.classList.remove("clockwise");
+  } else {
+    console.log("nothing happened");
+  }
+};
